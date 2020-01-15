2fd249eb2d145fba347013e04bda6e8f7bf3dbd8
Fabian <f.langer@search4solutions.nl>
2020-01-15 12:55:35 +0100

    Change q multiplier due to changes in action for poacher

--- DeDOL-master/poacher_cnn.py
+++ DeDOL-master/poacher_cnn.py
@@ -172,29 +172,24 @@ class Poacher(object):
         For building game tree usage
         '''
 
-        q_value_map = [1,1,1,1,1,
-                        1,1,1,1,1]
+        q_value_map = [1,1,1,1,1]
         
         # check up
         up = po_loc[1] + 1
         if len(animal_density)-1 > up and animal_density[po_loc[0]][up] <= 0:
             q_value_map[1] = 0
-            q_value_map[6] = 0
         # check down
         down = po_loc[1] - 1
         if 0 <= down and animal_density[po_loc[0]][down] <= 0:
             q_value_map[2] = 0
-            q_value_map[7] = 0
         # check left
         left = po_loc[0] - 1
         if 0 <= left and animal_density[left, po_loc[1]] <= 0:
             q_value_map[3] = 0
-            q_value_map[8] = 0
         # check right
         right = po_loc[0] + 1
         if len(animal_density[0])-1 > right and animal_density[right, po_loc[1]] <= 0:
             q_value_map[4] = 0
-            q_value_map[9] = 0
         return q_value_map
 
     def infer_action(self, sess, states, policy, po_loc, animal_density, epsilon=0.95, ):
