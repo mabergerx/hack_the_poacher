5b5424b2e02c2aba8738ec5dbfe798bc648aa3b3
Fabian <f.langer@search4solutions.nl>
2020-01-18 14:54:28 +0100

    Comment row num == 7 in args

--- DeDOL-master/DeDOL.py
+++ DeDOL-master/DeDOL.py
@@ -125,16 +125,16 @@ if args.row_num == 10:
         args.pa_episode_num = 2000
         args.po_episode_num = 2000
 
-if args.row_num == 7:
-    args.column_num = 7
-    args.max_time = 100
-    args.pa_initial_lr = 1e-4
-    args.po_initial_lr = 5e-5
-    args.pa_replay_buffer_size = 200000
-    args.po_replay_buffer_size = 100000
-    if args.po_location is not None:
-        args.pa_episode_num = 500
-        args.po_episode_num = 500
+# if args.row_num == 7:
+#     args.column_num = 7
+#     args.max_time = 100
+#     args.pa_initial_lr = 1e-4
+#     args.po_initial_lr = 5e-5
+#     args.pa_replay_buffer_size = 200000
+#     args.po_replay_buffer_size = 100000
+#     if args.po_location is not None:
+#         args.pa_episode_num = 10000
+#         args.po_episode_num = 10000
 
 elif args.row_num == 5:
     args.column_num = 5
